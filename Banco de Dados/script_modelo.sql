74a3de9bf8cfbbd791d34f65e3f9f4629aa78bd5
yagoprssantos <yagprogramr@gmail.com>
2023-11-16 18:32:15 -0300

    Add columns to tb_condominio and tb_usuario tables

--- Banco de Dados/script_modelo.sql
+++ Banco de Dados/script_modelo.sql
@@ -6,9 +6,11 @@ USE `condominio_ocorrencias_db`;
 CREATE TABLE tb_condominio (
  `id_condominio` INT AUTO_INCREMENT PRIMARY KEY,
  `nome_condominio` VARCHAR(100) NOT NULL,
- `qtd_apartamentos` INT NOT NULL,
+ `qtd_moradores` INT NOT NULL,
  `cep_condominio` VARCHAR(8) NOT NULL,
- `endereco_condominio` VARCHAR(150) NOT NULL
+ `endereco_condominio` VARCHAR(150) NOT NULL,
+ `id_administrador_fk` INT NOT NULL,
+  FOREIGN KEY (id_administrador_fk) REFERENCES usuario(id_usuario)
 );
 
 
@@ -19,21 +21,24 @@ create table `tb_usuario`(
  `nome_usuario` VARCHAR(150) NOT NULL,
  `email_usuario` VARCHAR(90) NOT NULL,
  `senha_usuario` VARCHAR(90) NOT NULL,
- `endereco_usuario` VARCHAR(150),
+ `cep_usuario` VARCHAR(150) NOT NULL,
  `telefone_usuario` VARCHAR(11) NOT NULL,
+ `tipo_usuario` ENUM('morador', 'administração') NOT NULL,
  `condominio_id_fk` INT NOT NULL,
   FOREIGN KEY (condominio_id_fk) REFERENCES condominio(id_condominio)
 );
 
 
--- Tabela de Órgãos Governamentais
-CREATE TABLE tb_orgaos_governamentais (
-  `id_orgaos_governamentais` INT AUTO_INCREMENT PRIMARY KEY,
-  `nome_orgaos_governamentais` VARCHAR(100),
-  `estado_orgaos_governamentais` CHAR(2) NOT NULL,
-  `tipo_orgaos_governamentais` ENUM('municipal', 'estadual', 'federal') NOT NULL,
-  `email_orgaos_governamentais` VARCHAR(50) NOT NULL,
-  `senha_orgaos_governamentais` VARCHAR(50) NOT NULL
+-- Criar tabela de adm?
+-- Tabela de Administradores
+CREATE TABLE tb_administrador (
+ `id_administrador` VARCHAR(100) NOT NULL,
+ `email_administrador` INT AUTO_INCREMENT PRIMARY KEY,
+ `nome_administrador` VARCHAR(50) NOT NULL,
+ `senha_administrador` VARCHAR(50) NOT NULL,
+ `telefone_administrador` VARCHAR(11) NOT NULL,
+ `condominio_id_fk` INT NOT NULL,
+  FOREIGN KEY (condominio_id_fk) REFERENCES condominio(id_condominio)
 );
 
 
@@ -41,17 +46,16 @@ CREATE TABLE tb_orgaos_governamentais (
 CREATE TABLE tb_ocorrencia (
  `id_ocorrencia` INT AUTO_INCREMENT PRIMARY KEY,
  `tipo_ocorrencia` ENUM('lixo', 'buraco em pista', 'assalto', 'bueiro sem proteção', 'placa danificada', 'banco danificado', 'calçada danificada', 'outros') NOT NULL,
- `descricao_ocorrencia` TEXT NOT NULL,
- `localizacao_ocorrencia` VARCHAR(60) NOT NULL,
+ `descricao_ocorrencia` VARCHAR(255) NOT NULL,
+ `localizacao_ocorrencia` VARCHAR(75) NOT NULL,
  `data_ocorrencia` DATETIME NOT NULL,
  `status_ocorrencia` ENUM('ocorrência resolvida', 'ocorrência em resolução', 'ocorrência não resolvida') NOT NULL,
+ `aprovacao_ocorrencia` INT NOT NULL,
  `usuario_id_fk` INT NOT NULL,
  `condominio_id_fk` INT NOT NULL,
- `orgao_id_fk` INT NOT NULL,
  `anexo_id_fk` INT NOT NULL,
  FOREIGN KEY (usuario_id_fk) REFERENCES usuario(id_usuario),
  FOREIGN KEY (condominio_id_fk) REFERENCES condominio(id_condominio),
- FOREIGN KEY (orgao_id_fk) REFERENCES orgaos_governamentais(id_orgaos_governamentais),
  FOREIGN KEY (ocorrencia_id_fk) REFERENCES ocorrencia(id)
 );
 
