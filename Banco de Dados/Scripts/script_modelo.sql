b6e51e208901915db7406282b3eb79c603115acc
yagoprssantos <yagprogramr@gmail.com>
2023-12-02 13:06:44 -0300

    Script modelo update

--- Banco de Dados/Scripts/script_modelo.sql
+++ Banco de Dados/Scripts/script_modelo.sql
@@ -15,41 +15,41 @@ CREATE TABLE tb_condominio (
 
 -- Tabela de Usuários
 CREATE TABLE tb_usuario (
- `cpf_usuario` VARCHAR(11) NOT NULL,
+ `id_usuario` INT AUTO_INCREMENT PRIMARY KEY,
+ `cpf_usuario` VARCHAR(14) NOT NULL,
  `nome_usuario` VARCHAR(150) NOT NULL,
  `email_usuario` VARCHAR(90) NOT NULL,
  `senha_usuario` VARCHAR(90) NOT NULL,
- `cep_usuario` VARCHAR(10) NOT NULL,
+ `cep_usuario` VARCHAR(9) NOT NULL,
  `telefone_usuario` VARCHAR(14) NOT NULL,
- `tipo_usuario` ENUM('morador', 'visitante', 'administrador') NOT NULL,
  `condominio_id_fk` INT NOT NULL,
-  PRIMARY KEY (`cpf_usuario`, `condominio_id_fk`),
   FOREIGN KEY (condominio_id_fk) REFERENCES tb_condominio(`id_condominio`)
 );
 
 
--- Tabela de Administradores dos Condomínios
-CREATE TABLE tb_condominio_administrador (
+-- Tabela dos Usuários de cada Condomínio
+CREATE TABLE tb_condominio_usuario (
+ `id_usuario` INT,
  `id_condominio` INT,
- `cpf_administrador` VARCHAR(11),
- PRIMARY KEY (`id_condominio`, `cpf_administrador`),
- FOREIGN KEY (`id_condominio`) REFERENCES tb_condominio(`id_condominio`),
- FOREIGN KEY (`cpf_administrador`) REFERENCES tb_usuario(`cpf_usuario`)
+ `tipo_usuario` ENUM('Morador', 'Visitante', 'Administrador') NOT NULL,
+ PRIMARY KEY (`id_usuario`, `id_condominio`),
+ FOREIGN KEY (`id_usuario`) REFERENCES tb_usuario(`id_usuario`),
+ FOREIGN KEY (`id_condominio`) REFERENCES tb_condominio(`id_condominio`)
 );
 
 
 -- Tabela de Ocorrências
 CREATE TABLE tb_ocorrencia (
  `id_ocorrencia` INT AUTO_INCREMENT PRIMARY KEY,
- `tipo_ocorrencia` ENUM('Vazamento de Agua','Falha Eletrica','Vandalismo','Furtos ou Roubos','Ruidos Excessivos','Comportamento Pertubador','Reparos Estruturais','Velocidade Excessiva','Estacionamento Inadequado','Caes Soltos','Limpeza Relacionada a Animais') NOT NULL,
+ `tipo_ocorrencia` ENUM('Vazamento de Água','Falha Elétrica','Vandalismo','Furtos ou Roubos','Ruídos Excessivos','Comportamento Pertubador','Reparos Estruturais','Velocidade Excessiva','Estacionamento Inadequado','Cães Soltos','Limpeza Relacionada a Animais') NOT NULL,
  `descricao_ocorrencia` VARCHAR(255) NOT NULL,
  `localizacao_ocorrencia` VARCHAR(75) NOT NULL,
  `data_ocorrencia` DATETIME NOT NULL,
- `status_ocorrencia` ENUM('ocorrência resolvida', 'ocorrência em resolução', 'ocorrência não resolvida') NOT NULL,
+ `status_ocorrencia` ENUM('Ocorrência Resolvida', 'Ocorrência em Resolução', 'Ocorrência não Resolvida') NOT NULL,
  `aprovacao_ocorrencia` INT NOT NULL,
- `usuario_cpf_fk` VARCHAR(11) NOT NULL,
+ `usuario_id_fk` INT NOT NULL,
  `condominio_id_fk` INT NOT NULL,
- FOREIGN KEY (`usuario_cpf_fk`) REFERENCES tb_usuario(`cpf_usuario`),
+ FOREIGN KEY (`usuario_id_fk`) REFERENCES tb_usuario(`id_usuario`),
  FOREIGN KEY (`condominio_id_fk`) REFERENCES tb_condominio(`id_condominio`)
 );
 
@@ -71,8 +71,8 @@ CREATE TABLE tb_comentario (
   `id_comentario` INT AUTO_INCREMENT PRIMARY KEY,
   `comentario` TEXT NOT NULL,
   `data_comentario` DATETIME NOT NULL,
-  `usuario_cpf_fk` VARCHAR(11) NOT NULL,
+  `usuario_id_fk` INT NOT NULL,
   `ocorrencia_id_fk` INT NOT NULL,
-  FOREIGN KEY (`usuario_cpf_fk`) REFERENCES `tb_usuario` (`cpf_usuario`),
+  FOREIGN KEY (`usuario_id_fk`) REFERENCES `tb_usuario` (`id_usuario`),
   FOREIGN KEY (`ocorrencia_id_fk`) REFERENCES `tb_ocorrencia` (`id_ocorrencia`)
 );
